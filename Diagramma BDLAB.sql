b60ef9647c67b2c9bb66b0664d4f6c4ba77c016c
Giacomo <94630562+SirFuryy@users.noreply.github.com>
2023-10-03 21:32:28 +0200

    Update Diagramma BDLAB.sql

--- Diagramma BDLAB.sql
+++ Diagramma BDLAB.sql
@@ -1,149 +1,152 @@
-CREATE TABLE "Segreteria" (
-  "id_segreteria" smallserial PRIMARY KEY,
-  "nome" varchar(20) NOT NULL,
-  "cognome" varchar(30) NOT NULL,
-  "email" varchar(30) UNIQUE NOT NULL,
-  "password" varchar(16) NOT NULL,
-  "data_nascita" date NOT NULL,
-  "sesso" varchar(7) NOT NULL,
-  "residenza" varchar(70) NOT NULL,
-  "telefono" varchar(17) NOT NULL,
-  "codice_fiscale" varchar(16) UNIQUE NOT NULL,
-  "domicilio" varchar(70),
-  "primo_anno" smallint,
-  "dipartimento" varchar(30)
+CREATE TABLE Segreteria (
+  id_segreteria smallserial PRIMARY KEY,
+  nome varchar(20) NOT NULL,
+  cognome varchar(30) NOT NULL,
+  email varchar(50) UNIQUE NOT NULL,
+  password varchar(16) NOT NULL,
+  data_nascita date NOT NULL,
+  sesso varchar(7) NOT NULL,
+  residenza varchar(70) NOT NULL,
+  telefono varchar(17) NOT NULL,
+  codice_fiscale char(16) UNIQUE NOT NULL,
+  domicilio varchar(70),
+  primo_anno smallint,
+  dipartimento varchar(30)
 );
 
-CREATE TABLE "Docente" (
-  "id_docente" smallserial PRIMARY KEY,
-  "nome" varchar(20) NOT NULL,
-  "cognome" varchar(30) NOT NULL,
-  "email" varchar(30) UNIQUE NOT NULL,
-  "password" varchar(16) NOT NULL,
-  "data_nascita" date NOT NULL,
-  "sesso" varchar(7) NOT NULL,
-  "residenza" varchar(70) NOT NULL,
-  "telefono" varchar(17) NOT NULL,
-  "codice_fiscale" varchar(16) UNIQUE NOT NULL,
-  "domicilio" varchar(70),
-  "primo_anno" smallint,
-  "ufficio" varchar(6)
+CREATE TABLE Docente (
+  id_docente smallserial PRIMARY KEY,
+  nome varchar(20) NOT NULL,
+  cognome varchar(30) NOT NULL,
+  email varchar(50) UNIQUE NOT NULL,
+  password varchar(16) NOT NULL,
+  data_nascita date NOT NULL,
+  sesso varchar(7) NOT NULL,
+  residenza varchar(70) NOT NULL,
+  telefono varchar(17) NOT NULL,
+  codice_fiscale char(16) UNIQUE NOT NULL,
+  domicilio varchar(70),
+  primo_anno smallint,
+  ufficio varchar(6)
 );
 
-CREATE TABLE "Studente" (
-  "matricola" char(6) PRIMARY KEY,
-  "nome" varchar(20) NOT NULL,
-  "cognome" varchar(30) NOT NULL,
-  "email" varchar(30) UNIQUE NOT NULL,
-  "password" varchar(16) NOT NULL,
-  "data_nascita" date NOT NULL,
-  "sesso" varchar(7) NOT NULL,
-  "residenza" varchar(70) NOT NULL,
-  "telefono" varchar(17) NOT NULL,
-  "codice_fiscale" varchar(16) UNIQUE NOT NULL,
-  "domicilio" varchar(70),
-  "data_iscrizione" timestamp NOT NULL,
-  "corso_laurea" char(6) NOT NULL
+CREATE TABLE Studente (
+  matricola char(6) PRIMARY KEY,
+  nome varchar(20) NOT NULL,
+  cognome varchar(30) NOT NULL,
+  email varchar(50) UNIQUE NOT NULL,
+  password varchar(16) NOT NULL,
+  data_nascita date NOT NULL,
+  sesso varchar(7) NOT NULL,
+  residenza varchar(70) NOT NULL,
+  telefono varchar(10) NOT NULL,
+  codice_fiscale char(16) UNIQUE NOT NULL,
+  domicilio varchar(70),
+  data_iscrizione timestamp NOT NULL,
+  corso_laurea char(6) NOT NULL
 );
 
-CREATE TABLE "Storico_studente" (
-  "matricola" char(6) PRIMARY KEY,
-  "nome" varchar(20) NOT NULL,
-  "cognome" varchar(30) NOT NULL,
-  "email" varchar(30) UNIQUE NOT NULL,
-  "password" varchar(16) NOT NULL,
-  "data_nascita" date NOT NULL,
-  "sesso" varchar(7) NOT NULL,
-  "residenza" varchar(70) NOT NULL,
-  "telefono" varchar(17) NOT NULL,
-  "codice_fiscale" varchar(16) UNIQUE NOT NULL,
-  "domicilio" varchar(70),
-  "data_iscrizione" timestamp NOT NULL,
-  "data_inizo_inattivita" timestamp NOT NULL,
-  "corso_laurea" char(6) NOT NULL
+CREATE TABLE Storico_studente (
+  matricola char(6) PRIMARY KEY,
+  nome varchar(20) NOT NULL,
+  cognome varchar(30) NOT NULL,
+  email varchar(50) UNIQUE NOT NULL,
+  password varchar(16) NOT NULL,
+  data_nascita date NOT NULL,
+  sesso varchar(7) NOT NULL,
+  residenza varchar(70) NOT NULL,
+  telefono varchar(10) NOT NULL,
+  codice_fiscale char(16) UNIQUE NOT NULL,
+  domicilio varchar(70),
+  data_iscrizione timestamp NOT NULL,
+  data_inizo_inattivita timestamp NOT NULL,
+  corso_laurea char(6) NOT NULL
 );
 
-CREATE TABLE "Corso" (
-  "codice_corso" char(6) PRIMARY KEY,
-  "nome" varchar(40) UNIQUE NOT NULL,
-  "tipologia" varchar(10) NOT NULL,
-  "descrizione" text NOT NULL
+CREATE TABLE Corso (
+  codice_corso char(6) PRIMARY KEY,
+  nome varchar(40) UNIQUE NOT NULL,
+  tipologia varchar(10) NOT NULL,
+  descrizione text NOT NULL
 );
 
-CREATE TABLE "Insegnamenti" (
-  "codice_insegnamento" char(6) PRIMARY KEY,
-  "nome" varchar(40) UNIQUE NOT NULL,
-  "anno" smallint NOT NULL,
-  "descrizione" text NOT NULL
+CREATE TABLE Insegnamenti (
+  codice_insegnamento char(6) PRIMARY KEY,
+  nome varchar(40) UNIQUE NOT NULL,
+  anno smallint NOT NULL,
+  descrizione text NOT NULL
 );
 
-CREATE TABLE "Calendario" (
-  "id_esame" serial PRIMARY KEY,
-  "cod_insegnamento" char(6) NOT NULL,
-  "data" date NOT NULL,
-  "posti" smallint NOT NULL,
-  "aula" varchar(20) NOT NULL
+CREATE TABLE Calendario (
+  id_esame serial PRIMARY KEY,
+  cod_insegnamento char(6) NOT NULL,
+  data date NOT NULL,
+  posti smallint NOT NULL,
+  aula varchar(20) NOT NULL
 );
 
-CREATE TABLE "Piano_didattico" (
-  "cod_insegnamento" char(6) NOT NULL,
-  "cod_corso" char(6) NOT NULL
+CREATE TABLE Piano_didattico (
+  cod_insegnamento char(6) NOT NULL,
+  cod_corso char(6) NOT NULL
 );
 
-CREATE TABLE "Insegna" (
-  "id_docente" smallserial NOT NULL,
-  "cod_insegnamento" char(6) NOT NULL
+CREATE TABLE Insegna (
+  id_docente smallserial NOT NULL,
+  cod_insegnamento char(6) NOT NULL
 );
 
-CREATE TABLE "Propeduticita" (
-  "cod_insegnamento" char(6) NOT NULL,
-  "cod_ins_proped" char(6) NOT NULL
+CREATE TABLE Propeduticita (
+  cod_insegnamento char(6) NOT NULL,
+  cod_ins_proped char(6) NOT NULL
 );
 
-CREATE TABLE "Appelli" (
-  "matricola_stud" char(6) NOT NULL,
-  "id_esame" serial NOT NULL,
-  "voto" smallint NOT NULL
+CREATE TABLE Appelli (
+  matricola_stud char(6) NOT NULL,
+  id_esame serial NOT NULL,
+  voto smallint NOT NULL
 );
 
-CREATE TABLE "Storico_appelli" (
-  "matricola_stud" char(6) NOT NULL,
-  "id_esame" serial NOT NULL,
-  "voto" smallint NOT NULL
+CREATE TABLE Storico_appelli (
+  matricola_stud char(6) NOT NULL,
+  id_esame serial NOT NULL,
+  voto smallint NOT NULL
 );
 
-CREATE INDEX ON "Piano_didattico" ("cod_corso", "cod_insegnamento");
+CREATE INDEX ON Piano_didattico (cod_corso, cod_insegnamento);
 
-CREATE INDEX ON "Insegna" ("id_docente", "cod_insegnamento");
+CREATE INDEX ON Insegna (id_docente, cod_insegnamento);
 
-CREATE INDEX ON "Propeduticita" ("cod_insegnamento", "cod_ins_proped");
+CREATE INDEX ON Propeduticita (cod_insegnamento, cod_ins_proped);
 
-CREATE INDEX ON "Appelli" ("matricola_stud", "id_esame");
+CREATE INDEX ON Appelli (matricola_stud, id_esame);
 
-CREATE INDEX ON "Storico_appelli" ("matricola_stud", "id_esame");
+CREATE INDEX ON Storico_appelli (matricola_stud, id_esame);
 
-ALTER TABLE "Studente" ADD FOREIGN KEY ("corso_laurea") REFERENCES "Corso" ("codice_corso"); /* prima da cambiare */
+ALTER TABLE Studente ADD FOREIGN KEY (corso_laurea) REFERENCES Corso (codice_corso); /* prima da cambiare */
 
-ALTER TABLE "Piano_didattico" ADD FOREIGN KEY ("cod_corso") REFERENCES "Corso" ("codice_corso");
+ALTER TABLE Piano_didattico ADD FOREIGN KEY (cod_corso) REFERENCES Corso (codice_corso);
 
-ALTER TABLE "Piano_didattico" ADD FOREIGN KEY ("cod_insegnamento") REFERENCES "Insegnamenti" ("codice_insegnamento");
+ALTER TABLE Piano_didattico ADD FOREIGN KEY (cod_insegnamento) REFERENCES Insegnamenti (codice_insegnamento);
 
-ALTER TABLE "Insegna" ADD FOREIGN KEY ("cod_insegnamento") REFERENCES "Insegnamenti" ("codice_insegnamento");
+ALTER TABLE Insegna ADD FOREIGN KEY (cod_insegnamento) REFERENCES Insegnamenti (codice_insegnamento);
 
-ALTER TABLE "Insegna" ADD FOREIGN KEY ("id_docente") REFERENCES "Docente" ("id_docente");
+ALTER TABLE Insegna ADD FOREIGN KEY (id_docente) REFERENCES Docente (id_docente);
 
-ALTER TABLE "Propeduticita" ADD FOREIGN KEY ("cod_insegnamento") REFERENCES "Insegnamenti" ("codice_insegnamento");
+ALTER TABLE Propeduticita ADD FOREIGN KEY (cod_insegnamento) REFERENCES Insegnamenti (codice_insegnamento);
 
-ALTER TABLE "Propeduticita" ADD FOREIGN KEY ("cod_ins_proped") REFERENCES "Insegnamenti" ("codice_insegnamento");
+ALTER TABLE Propeduticita ADD FOREIGN KEY (cod_ins_proped) REFERENCES Insegnamenti (codice_insegnamento);
 
-ALTER TABLE "Calendario" ADD FOREIGN KEY ("cod_insegnamento") REFERENCES "Insegnamenti" ("codice_insegnamento"); /* seconda da cambiare */
+ALTER TABLE Calendario ADD FOREIGN KEY (cod_insegnamento) REFERENCES Insegnamenti (codice_insegnamento); /* seconda da cambiare */
 
-ALTER TABLE "Appelli" ADD FOREIGN KEY ("matricola_stud") REFERENCES "Studente" ("matricola");
+ALTER TABLE Appelli ADD FOREIGN KEY (matricola_stud) REFERENCES Studente (matricola);
 
-ALTER TABLE "Appelli" ADD FOREIGN KEY ("id_esame") REFERENCES "Calendario" ("id_esame");
+ALTER TABLE Appelli ADD FOREIGN KEY (id_esame) REFERENCES Calendario (id_esame);
 
-ALTER TABLE "Storico_appelli" ADD FOREIGN KEY ("matricola_stud") REFERENCES "Storico_studente" ("matricola");
+ALTER TABLE Storico_appelli ADD FOREIGN KEY (matricola_stud) REFERENCES Storico_studente (matricola);
 
-ALTER TABLE "Storico_appelli" ADD FOREIGN KEY ("id_esame") REFERENCES "Calendario" ("id_esame");
+ALTER TABLE Storico_appelli ADD FOREIGN KEY (id_esame) REFERENCES Calendario (id_esame);
 
-ALTER TABLE "Storico_studente" ADD FOREIGN KEY ("corso_laurea") REFERENCES "Corso" ("codice_corso"); /* terza da cambiare */
+ALTER TABLE Storico_studente ADD FOREIGN KEY (corso_laurea) REFERENCES Corso (codice_corso); /* terza da cambiare */
+
+
+/*INSERT INTO segreteria VALUES (1, 'Giacomo', 'Guidi', 'giacomo.guidi@di.unimi.it', 11111111, '2002/06/20', 'maschio', 'b', 3336744745, 'gdugcm02h20h294g', 'b', 2010, 'informatica')*/
